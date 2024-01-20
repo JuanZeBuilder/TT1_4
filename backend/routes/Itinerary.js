bb6fba740c0e98e90b26551671d9447fb62b2e20
ishaa-m <isha09091@gmail.com>
2024-01-20 13:38:13 +0800

    feature done

--- backend/routes/Itinerary.js
+++ backend/routes/Itinerary.js
@@ -4,5 +4,6 @@ module.exports = (app) => {
   var router = require("express").Router();
   router.get("/:userId", itinerary.getItineraryByUserId);
   router.post("/", itinerary.createItinerary);
+  router.delete("/:itineraryId", itinerary.deleteItinerary);
   app.use("/api/itinerary", router);
 };
